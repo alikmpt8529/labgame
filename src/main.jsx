4e813eff41ac7ab926ff282a2f16a0bc2c3e0887
alikmpt8529 <m80693827@gmail.com>
2025-06-06 11:31:02 +0900

    名称変更

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './level2Screen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
